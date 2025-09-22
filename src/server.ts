bfc5179d229a4c5d90562f86d0a04a9a4199efa0
Prem Sagar <psagar172@gmail.com>
2025-09-22 13:59:45 +0530

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}
+    Visit http://localhost:${PORT} to see the result`);
+});
